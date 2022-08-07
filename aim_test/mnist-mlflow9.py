38c4d9326b4c5b908ce58bfd139e74827ec75d35
Dongki Lee <waitlee@pubg.com>
2022-08-07 22:11:55 +0900

    add test method

---
+++ aim_test/mnist-mlflow9.py
@@ -0,0 +1,137 @@
+import mlflow
+
+import torch
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+import torch.nn.init
+
+from mlflow_migration import parse_mlflow_logs
+
+
+while True:
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    torch.manual_seed(777)
+
+    if device == 'cuda':
+        torch.cuda.manual_seed_all(777)
+
+    remote_server_uri = "http://mlflow.krafton-ai.com:5000"
+    mlflow.set_tracking_uri(remote_server_uri)
+    # Choose your experiment
+    mlflow.set_experiment("mnist-test")
+    mlflow.set_tags({"mlflow.user": "synclee", "mlflow.runName": "test-02"})
+
+    learning_rate = 0.002
+    training_epochs = 0
+    batch_size = 200
+
+    mlflow.log_params({
+        'batch_size': batch_size,
+        'training_epochs': training_epochs,
+        'learning_rate': learning_rate
+    })
+
+    mnist_train = dsets.MNIST(root='./data/MNIST_data/',  # 다운로드 경로 지정
+                              train=True,  # True를 지정하면 훈련 데이터로 다운로드
+                              transform=transforms.ToTensor(),  # 텐서로 변환
+                              download=True)
+
+    mnist_test = dsets.MNIST(root='./data/MNIST_data/',  # 다운로드 경로 지정
+                             train=False,  # False를 지정하면 테스트 데이터로 다운로드
+                             transform=transforms.ToTensor(),  # 텐서로 변환
+                             download=True)
+
+    data_loader = torch.utils.data.DataLoader(dataset=mnist_train,
+                                              batch_size=batch_size,
+                                              shuffle=True,
+                                              drop_last=True)
+
+
+    class CNN(torch.nn.Module):
+
+        def __init__(self):
+            super(CNN, self).__init__()
+            self.keep_prob = 0.5
+            self.layer1 = torch.nn.Sequential(
+                torch.nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+                torch.nn.ReLU(),
+                torch.nn.MaxPool2d(kernel_size=2, stride=2))
+
+            self.layer2 = torch.nn.Sequential(
+                torch.nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+                torch.nn.ReLU(),
+                torch.nn.MaxPool2d(kernel_size=2, stride=2))
+
+            self.layer3 = torch.nn.Sequential(
+                torch.nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+                torch.nn.ReLU(),
+                torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=1))
+
+            self.fc1 = torch.nn.Linear(4 * 4 * 128, 625, bias=True)
+            torch.nn.init.xavier_uniform_(self.fc1.weight)
+            self.layer4 = torch.nn.Sequential(
+                self.fc1,
+                torch.nn.ReLU(),
+                torch.nn.Dropout(p=1 - self.keep_prob))
+
+            self.fc2 = torch.nn.Linear(625, 10, bias=True)
+            torch.nn.init.xavier_uniform_(self.fc2.weight)
+
+        def forward(self, x):
+            out = self.layer1(x)
+            out = self.layer2(out)
+            out = self.layer3(out)
+            out = out.view(out.size(0), -1)  # Flatten them for FC
+            out = self.layer4(out)
+            out = self.fc2(out)
+            return out
+
+
+    model = CNN().to(device)
+
+    criterion = torch.nn.CrossEntropyLoss().to(device)  # 비용 함수에 소프트맥스 함수 포함되어져 있음.
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    total_batch = len(data_loader)
+    print('총 배치의 수 : {}'.format(total_batch))
+
+    for epoch in range(training_epochs):
+        avg_cost = 0
+
+        for X, Y in data_loader:  # 미니 배치 단위로 꺼내온다. X는 미니 배치, Y느 ㄴ레이블.
+            # image is already size of (28x28), no reshape
+            # label is not one-hot encoded
+            X = X.to(device)
+            Y = Y.to(device)
+
+            optimizer.zero_grad()
+            hypothesis = model(X)
+            cost = criterion(hypothesis, Y)
+            cost.backward()
+            optimizer.step()
+
+            avg_cost += cost / total_batch
+
+        print('[Epoch: {:>4}] cost = {:>.9}'.format(epoch + 1, avg_cost))
+
+        # MLFlow code
+        mlflow.log_metric(key='cost', value=avg_cost, step=epoch + 1)
+
+    with torch.no_grad():
+        X_test = mnist_test.test_data.view(len(mnist_test), 1, 28, 28).float().to(device)
+        Y_test = mnist_test.test_labels.to(device)
+
+        prediction = model(X_test)
+        correct_prediction = torch.argmax(prediction, 1) == Y_test
+        accuracy = correct_prediction.float().mean()
+        print('Accuracy:', accuracy.item())
+
+        # MLFlow code
+        mlflow.log_metric(key='accuracy', value=accuracy.item())
+        mlflow.log_artifacts('./temp/')
+
+    run_id = mlflow.active_run().info.run_id
+    mlflow.end_run()
+    run = mlflow.get_run(run_id=run_id)
+    parse_mlflow_logs(tracking_uri=remote_server_uri, experiment_name='mnist-test', run=run)
